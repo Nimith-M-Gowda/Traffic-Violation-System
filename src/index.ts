9ff6a6c35746cc778d5e004acff8a0edc670a1a6
nimith <nimithbbz10@gmail.com>
2023-08-21 01:30:04 +0530

    chore (logs) : added logs

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,15 @@ export const handler = ((vehicleId: string, locationId: string) => {
 			vehicleId,
 			locationId
 		)
+		console.log(
+			`get Traffic Violated Vehicle Data : ${gettrafficViolatedVehicleData}`
+		)
 
 		// Step 2 : Query for vehicleType and OwnerPhoneNumber from RTO DB.
 		const { vehicleType, ownerPhoneNumber } = getVehicleInfo(vehicleId)
+		console.log(
+			`vehicleType : ${vehicleType}, ownerPhoneNumber : ${ownerPhoneNumber}`
+		)
 
 		//Step 3 : Calculate Speed for a given vehicle and query for Speed limit in the particular location from DB
 		const SpeedInfo = gettrafficViolatedVehicleData.map(eachViolatedData => {
@@ -35,12 +41,15 @@ export const handler = ((vehicleId: string, locationId: string) => {
 				vehicleSpeed
 			}
 		})
+		console.log('SpeedInfo :', SpeedInfo)
 
 		// Step 4 : Based on above speeding calculate how many times speed violation taken place
 		const TotalMistakeCount = getTotalViolationCount(SpeedInfo)
+		console.log('TotalMistakeCount', TotalMistakeCount)
 
 		//Step 5 : calculates fine amount based on speed violation
 		const fineAmount = calculateFineToBePaid(vehicleType, TotalMistakeCount)
+		console.log('fineAmount', fineAmount)
 
 		//Step 6 : If there is any fine amount, then SMS user
 		SMSOwner(
